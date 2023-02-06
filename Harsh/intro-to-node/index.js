b509e6ab968728e9ebc27ad72f6070e26337333a
Harsh sharma <70514943+Harshsharma836@users.noreply.github.com>
2023-02-06 11:46:52 +0530

    Add files via upload

---
+++ Harsh/intro-to-node/index.js
@@ -0,0 +1,11 @@
+// Node code for copying file data.
+
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt","file2.txt");
+
+
+//-------------------------------------------------
+
+const superheroes = require('superheroes');
+var a = superheroes.random();
+console.log(a);
